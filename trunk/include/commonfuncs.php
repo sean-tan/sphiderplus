d0bc706788db995ea8b30285a6ef0795cd95043a
intel352 <intel352@80f5ada6-0ff0-11de-9cb0-39e535855b64>
2009-03-13 17:22:25 +0000

    Correcting footer

--- trunk/include/commonfuncs.php
+++ trunk/include/commonfuncs.php
@@ -900,7 +900,7 @@ function replace_ampersand($str) {
     	echo "<p class='stats'>
             <!-- The following should only be removed if you contribute (donate) to the Sphider-plus project.
             Note that this is a requirement under the GPL licensing agreement, which Sphider-plus acknowledges. -->	
-    		<a href='https://sourceforge.net/project/showfiles.php?group_id=214642' title='Link: Visit Sphider-plus site in new window' target='rel'>Visit
+    		<a href='http://code.google.com/p/sphiderplus/' title='Link: Visit Sphider-plus site in new window' target='rel'>Visit
     		<img class='mid' src='sphider-logo.png' alt='Visit Sphider site in new window' height='15' width='80'
     		/> -plus
             </a>                
